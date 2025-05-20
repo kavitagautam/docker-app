df9e30e684a19100622c2b0739b67149cb5b3aca
kavita <kavitgautam014@gmail.com>
2025-05-20 14:22:03 +0545

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, docker-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello Everyone, This is Kavita docker-app');
   });
 });
